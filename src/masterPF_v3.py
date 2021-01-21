bad86441e1eb56536790feed37aaac37e1e73d87
ricardo <ricardo.reibsch@rl-stiftung.de>
2021-01-21 15:44:24 +0100

    Add changes

--- src/masterPF_v3.py
+++ src/masterPF_v3.py
@@ -24,10 +24,10 @@ net_name = ["kerber_rural_1", #0
             "simbench_urban_6"]  #12
 
 # define timescope and timestepwidth
-
-start_time = '2017-01-01 00:01:00+01:00'
-end_time = '2017-12-31 23:59:00+01:00'
-t_freq = 'T'
+# TODO: create dict
+start_time = '2017-05-17 00:01:00+01:00'
+end_time = '2017-05-17 23:59:00+01:00'
+t_freq = '10T'
 '''
 start_time = '2017-05-17 00:00:00+02:00'
 end_time = '2017-05-17 23:59:00+02:00'
@@ -49,11 +49,11 @@ e.run_pf_timeseries()
 ############# ANALISE OUTPUT DATA ##############
 ################################################
 
-tda.calculate_relevant_outputdata(e.output_dir, t_freq)
-tda.calculate_net_problems(e.output_dir)
-tda.plot_generation_consumption_as_heat_map(e.output_dir)
-tda.plot_grid_issus_over_power(e.output_dir)
-tda.plot_residualload(e.output_dir)
+#tda.calculate_relevant_outputdata(e.output_dir, t_freq)
+#tda.calculate_net_problems(e.output_dir)
+#tda.plot_generation_consumption_as_heat_map(e.output_dir)
+#tda.plot_grid_issus_over_power(e.output_dir)
+#tda.plot_residualload(e.output_dir)
 #tda.plot_colorbar_seaborn(e.output_dir, start_time, end_time)
 #tda.plot_input_data()
 #tda.plot_net_res(net)
