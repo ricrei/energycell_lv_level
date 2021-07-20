d4a75c5d92b99c111bf32e6cde6be35bba74d22b
ricardo <ricardo.reibsch@rl-stiftung.de>
2021-07-20 16:56:33 +0200

    Add new time_scope autumn

--- src/main_EnergyCell.py
+++ src/main_EnergyCell.py
@@ -10,8 +10,8 @@ import tools.evaluation.EvaluationAllCases as EvaAllCases
 
 ##########################################
 ### Define timescope and timestepwidth ###
-time_scope = { 'start_time' : '2017-05-26 00:00:00+02:00',
-               'end_time'   : '2017-05-26 23:00:00+02:00',
+time_scope = { 'start_time' : '2017-10-21 00:00:00+02:00',
+               'end_time'   : '2017-10-27 00:00:00+02:00',
                't_freq'     : '1H'
              }
 
@@ -27,6 +27,12 @@ time_scope_summer = { 'start_time' : '2017-05-26 00:00:00+02:00',
                       'end_time'   : '2017-06-02 00:00:00+02:00',
                       't_freq'     : '1T',
                       'name'       : 'summer'
+                    }
+
+time_scope_autumn = { 'start_time' : '2017-10-21 00:00:00+02:00',
+                      'end_time'   : '2017-10-27 00:00:00+02:00',
+                      't_freq'     : '1T',
+                      'name'       : 'autumn'
                     }
 
 time_scope = time_scope
@@ -45,7 +51,7 @@ time_scope = time_scope
 ## 2: Grid-oriented feed-in damping (only in scenario[0] 6, 7, 8)
 # Third digit:
 ## 0: No Curtailment, 1: Curtailed Operation (residualload oriented)
-scenario = [6, 1, 0]
+scenario = [4, 0, 0]
 #######################
 
 ############################
