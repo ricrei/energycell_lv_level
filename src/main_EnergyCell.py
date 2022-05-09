67165ac3b18984f4705a5440fff1597cd9a0b5d9
ricardo <ricardo.reibsch@rl-stiftung.de>
2022-05-09 13:43:26 +0200

    One bfore merge

--- src/main_EnergyCell.py
+++ src/main_EnergyCell.py
@@ -152,8 +152,8 @@ def run_single_simulation():
   # Initialize Evaluation
   e.initiate_evaluation()
 
-  e.eva.calculate_relevant_outputdata()
-  #e.eva.calculate_net_problems()
+  #e.eva.calculate_relevant_outputdata()
+  e.eva.calculate_net_problems()
 
   e.eva.plot_residualload(add_curtail=True, add_losses=False)
   #e.eva.plot_ev_soc()
@@ -163,10 +163,11 @@ def run_single_simulation():
   #e.eva.plot_grid_issus_over_time()
   #e.eva.plot_pv_active_power()
   #e.eva.plot_pv_reactive_power()
-  e.eva.plot_hp_soc()
-  e.eva.plot_hp_cop()
-  e.eva.plot_hp_active_power()
-  e.eva.plot_hp_demand_th()
+  #e.eva.plot_hp_soc()
+  #e.eva.plot_hp_cop()
+  #e.eva.plot_hp_active_power()
+  #e.eva.plot_hp_demand_th()
+  e.eva.plot_hp_eva_th()
   #e.eva.plot_bss_active_power()
   #e.eva.plot_soc()
   #e.eva.plot_bss_e_mwh()
